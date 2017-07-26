5290589d2aec992a3b391e5679e9481be3392ce3
Henrik Steen <henrist@henrist.net>
2017-07-26 02:08:27 +0200

    Add backward compat for generate-domains

--- dehydrated/generate-domains.sh
+++ dehydrated/generate-domains.sh
@@ -4,3 +4,7 @@
 # configuration which we will try to maintain certificates for.
 grep -rh "ssl_certificate\s*/opt/dehydrated/certs/" /etc/nginx | \
   sed 's/.*certs\/\(.*\)\/full.*/\1/' >/opt/dehydrated/domains.txt
+
+# Backward compatibility
+grep -rh "ssl_certificate\s*/opt/letsencrypt.sh/certs/" /etc/nginx | \
+  sed 's/.*certs\/\(.*\)\/full.*/\1/' >>/opt/dehydrated/domains.txt
